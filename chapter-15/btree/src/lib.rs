9f71495cedb0640536c7f4599ce50ba38801d791
Ildar Shamiev <shamief@yandex.ru>
2023-04-08 15:29:05 +0300

    style tests

--- chapter-15/btree/src/lib.rs
+++ chapter-15/btree/src/lib.rs
@@ -3,10 +3,14 @@ pub mod tree;
 
 #[cfg(test)]
 mod tests {
+    use rand::prelude::*;
     use rand::distributions::{Alphanumeric, DistString};
-    use super::*;
+    use super::node::*;
+    use super::tree::*;
 
-    #[derive(Clone, Default)]
+    const LEN: usize = 10;
+
+    #[derive(Clone, Default, Debug, PartialEq)]
     struct Value {
         id: usize,
         #[allow(dead_code)]
@@ -19,7 +23,7 @@ mod tests {
         }
     }
 
-    impl node::Identity for Value {
+    impl Identity for Value {
         fn id(&self) -> usize {
             self.id
         }
@@ -27,14 +31,51 @@ mod tests {
 
     #[test]
     fn btree_is_valid() {
-        let mut btree = tree::BTree::new(3);
-        for i in 0..10 {
+        let mut btree = BTree::new(3);
+        for i in 0..LEN {
             let s: String = Alphanumeric
                 .sample_string(&mut rand::thread_rng(), 7);
             let v = Value::new(i, &s);
             btree.add(v);
         }
-        assert_eq!(btree.length(), 10);
+        assert_eq!(btree.length(), LEN);
         assert!(btree.is_valid());
     }
+
+    #[test]
+    fn btree_found() {
+        let mut rng = rand::thread_rng();
+        let mut btree = BTree::new(4);
+        let mut index: Vec<usize> = (0..LEN).collect();
+        index.shuffle(&mut rng);
+        for i in index.iter() {
+            let s: String = Alphanumeric
+                .sample_string(&mut rng, 7); 
+            let v = Value::new(*i, &s);
+            btree.add(v);
+        }
+        index.shuffle(&mut rng);
+        for i in index.iter() {
+            assert_ne!(btree.find(*i), None);
+        }
+    }
+
+    #[test]
+    fn btree_not_found() {
+        let mut rng = rand::thread_rng();
+        let mut btree = BTree::new(4);
+        let mut index: Vec<usize> = (0..LEN).collect();
+        index.shuffle(&mut rng);
+        for i in index.iter() {
+            let s: String = Alphanumeric
+                .sample_string(&mut rng, 7); 
+            let v = Value::new(*i, &s);
+            btree.add(v);
+        }
+        index.shuffle(&mut rng);
+        for i in index.iter() {
+            assert_eq!(btree.find(*i + LEN), None);
+        }
+    }
+
 }
