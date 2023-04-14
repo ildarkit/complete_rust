d86e6070e88354a8d29cfcc96a1ef78e61ea11c5
Ildar Shamiev <shamief@yandex.ru>
2023-04-14 10:24:52 +0300

    rand dep, test

--- chapter-15/btree-one-pass/src/lib.rs
+++ chapter-15/btree-one-pass/src/lib.rs
@@ -3,11 +3,52 @@ pub mod btree;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use log::debug;
+    use rand::prelude::*;
+    use rand::distributions::{Alphanumeric, DistString};
+    use super::node::*;
+    use super::btree::*;
+
+    // const LEN: usize = 10000;
+
+    fn init_logger() {
+        let _ = env_logger::builder().is_test(true).try_init();
+    }
+
+    #[derive(Clone, Default, Debug, PartialEq)]
+    struct Data<U> {
+        key: U,
+        #[allow(dead_code)]
+        data: String,
+    }
+
+    impl<U> Data<U> {
+        fn new(key: U, data: &str) -> Self {
+            Self { key, data: data.to_owned() }
+        }
+    }
+
+    impl<U: Copy> Key<U> for Data<U> {
+        fn key(&self) -> U {
+            self.key
+        }
+    }
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn found_node() {
+        init_logger();
+        let mut rng = thread_rng();
+        let mut btree = BTree::new(3);
+
+        for c in "string".chars() {
+            let data = Data::new(c, &Alphanumeric.sample_string(&mut rng, 7));
+            btree.insert(data)
+        }
+        match btree.search('s') {
+            Some(NodePosition{node, pos: i, ..}) => {
+                debug!("\npos = {i}, node = {:#?}", node)
+            }
+            None => assert!(false, "node not found"),
+        }
     }
 }
