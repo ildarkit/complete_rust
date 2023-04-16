3693a54c2ecbb4ff673a9ab71d308034f467782a
Ildar Shamiev <shamief@yandex.ru>
2023-04-16 09:12:10 +0300

    add tests

--- chapter-15/btree-one-pass/src/lib.rs
+++ chapter-15/btree-one-pass/src/lib.rs
@@ -9,8 +9,6 @@ mod tests {
     use super::node::*;
     use super::btree::*;
 
-    // const LEN: usize = 10000;
-
     fn init_logger() {
         let _ = env_logger::builder().is_test(true).try_init();
     }
@@ -34,28 +32,91 @@ mod tests {
     }
 
     #[test]
+    fn search_in_empty_btree() {
+        let btree: BTree<char, Data<char>> = BTree::new(3); 
+        let search_char = 'a';
+        assert!(btree.search(search_char).is_none());
+    }
+
+    #[test]
+    fn not_found_node() {
+        let mut rng = thread_rng();
+        let mut btree = BTree::new(3);
+        let mut chars: Vec<char> = ('a'..'z').collect();
+        chars.shuffle(&mut rng);
+        let search_char = 'K';
+
+        for c in chars.iter() {
+            let d = Alphanumeric.sample_string(&mut rng, 7);
+            let data = Data::new(*c, &d);
+            btree.insert(data)
+        }
+        assert!(btree.search(search_char).is_none());
+    }
+
+    #[test]
     fn found_node() {
         init_logger();
-        const SEARCH_CHAR: char = 'm';
         let mut rng = thread_rng();
         let mut btree = BTree::new(3);
         let mut s = "test".to_owned();
+        let mut chars: Vec<char> = ('a'..'z').collect();
+        chars.shuffle(&mut rng);
+        let search_char = chars[rng.gen_range(0..chars.len())];
 
-        for c in "qwertyasdfglkjhzxcvmnbpoiu".chars() {
+        for c in chars.iter() {
             let d = Alphanumeric.sample_string(&mut rng, 7);
-            if c == SEARCH_CHAR {
+            if *c == search_char {
                 s = d.clone();
             }
-            let data = Data::new(c, &d);
-            debug!("\nvalue = {:?}", data);
+            let data = Data::new(*c, &d);
+            debug!("\ndata = {:?}", data);
             btree.insert(data)
         }
-        match btree.search(SEARCH_CHAR) {
+        match btree.search(search_char) {
             Some(NodePosition{node, pos, ..}) => {
-                debug!("\npos = {pos}, node = {:#?}", node);
-                assert_eq!(node.get_key(pos), Some(&Data{key: SEARCH_CHAR, data: s}));
+                debug!("\npos = {pos}, char = {search_char}, node = {:#?}", node);
+                assert_eq!(node.get_key(pos), Some(&Data{key: search_char, data: s}));
             }
             None => assert!(false, "node not found"),
+        }
+    }
+
+    #[test]
+    #[ignore = "long searching key test in btree on random input"]
+    fn found_node_stress() {
+        init_logger();
+        let mut rng = thread_rng();
+        let mut chars: Vec<char> = ('a'..'z').collect();
+
+        for i in 5..chars.len() {
+            for _ in 0..10000 {
+                let mut btree = BTree::new(3);
+                let mut s = "test".to_owned();
+                chars.shuffle(&mut rng);
+                let test_slice = &chars[..i];
+                let search_char = chars[rng.gen_range(0..i)];
+                for c in test_slice {
+                    let d = Alphanumeric.sample_string(&mut rng, 7);
+                    if *c == search_char {
+                        s = d.clone();
+                    }
+                    let data = Data::new(*c, &d);
+                    debug!("\nvalue = {:?}", data);
+                    btree.insert(data)
+                }
+                match btree.search(search_char) {
+                    Some(NodePosition{node, pos, ..}) => {
+                        debug!("\npos = {pos}, char = {search_char}, node = {:#?}", node);
+                        assert_eq!(node.get_key(pos),
+                            Some(&Data{key: search_char, data: s.clone()}),
+                            "\nerror: {search_char}, {test_slice:?}"
+                        );
+                    }
+                    None => assert!(false,
+                        "\nnot found {search_char} in {test_slice:?}"),
+                }
+            }
         }
     }
 }
